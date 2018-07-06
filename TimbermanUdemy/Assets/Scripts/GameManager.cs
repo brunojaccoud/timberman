cc20844f6905371bfb9acc6cce13abbfd4133b23
brunojaccoud <bruno_jaccoud@hotmail.com>
2018-07-06 00:18:11 -0300

    including timberman from unity

---
+++ TimbermanUdemy/Assets/Scripts/GameManager.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Game : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+		Debug.Log("Passo 1");
+		Debug.Log("Passo 2");
+		Debug.Log("Passo 3");
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
