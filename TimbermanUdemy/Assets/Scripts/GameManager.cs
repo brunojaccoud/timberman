22d621cfc2ea2edc6d69f929dcee6c2e51f2f71b
brunojaccoud <bruno_jaccoud@hotmail.com>
2018-07-06 00:25:27 -0300

    variaveis

--- TimbermanUdemy/Assets/Scripts/GameManager.cs
+++ TimbermanUdemy/Assets/Scripts/GameManager.cs
@@ -4,11 +4,15 @@ using UnityEngine;
 
 public class Game : MonoBehaviour {
 
+	//variaveis da tree
+	private float alturaTronco;
+	private float posicaoInicialY;
+	private int maxTroncos = 6;
+	private bool troncoSemGalho = false;
+
 	// Use this for initialization
 	void Start () {
-		Debug.Log("Passo 1");
-		Debug.Log("Passo 2");
-		Debug.Log("Passo 3");
+
 	}
 	
 	// Update is called once per frame
