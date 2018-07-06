189f673121abce1a39102b7d381e3c433bdbda6d
brunojaccoud <bruno_jaccoud@hotmail.com>
2018-07-06 19:41:45 -0300

    novas classes e blablabla

--- TimbermanUdemy/Assets/Scripts/GameManager.cs
+++ TimbermanUdemy/Assets/Scripts/GameManager.cs
@@ -10,24 +10,48 @@ public class GameManager : MonoBehaviour {
 
 
 	private float alturaTronco = 2.43f;
-	private float posicaoInicialY = -4f;
+	private float posicaoInicialY = -2.38f;
 	private int maxTroncos = 6;
 	private bool troncoSemGalho = false;
 
 	// Use this for initialization
 	void Start () {
 
+		InicializaTroncos();
 	}
 	
 	// Update is called once per frame
 	void Update () {
+
+		if (Input.GetButtonDown("Fire1")) {
+			CortaTronco();
+			ReposicionaTronco();
+		}
 		
 	}
 
 	void CriaTroncos(int posicao) {
 		GameObject tronco = Instantiate(troncoSemGalho ? troncos [Random.Range (0,3)] : troncos[0]);
-		tronco.transform.localPosition = new Vector3 (0f, posicaoInicialY + posicao * alturaTronco, 0f);
+		tronco.transform.localPosition = new Vector3 (-0.05f, posicaoInicialY + posicao * alturaTronco, 0f);
 		listaTroncos.Add(tronco);
 		troncoSemGalho = !troncoSemGalho;
+	}
+
+	void InicializaTroncos() {
+		for (int posicao = 0; posicao <= maxTroncos; posicao++) {
+			CriaTroncos(posicao);
+		}
+	}
+
+	void CortaTronco() {
+		Destroy (listaTroncos[0]);
+		listaTroncos.RemoveAt(0);
+	}
+
+	void ReposicionaTronco() {
+		for (int posicao = 0; posicao < listaTroncos.Count; posicao++) {
+			listaTroncos[posicao].transform.localPosition = new Vector3 (-0.05f, posicaoInicialY + posicao * alturaTronco, 0f);
+		}
+		CriaTroncos(maxTroncos);
 	}
 }
