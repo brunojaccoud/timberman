409e8b858f6c2d471c74171212d964ec3ba9f649
brunojaccoud <bruno_jaccoud@hotmail.com>
2018-07-08 00:28:30 -0300

    ajuste

--- TimbermanUdemy/Assets/Scripts/Player.cs
+++ TimbermanUdemy/Assets/Scripts/Player.cs
@@ -6,7 +6,7 @@ public class Player : MonoBehaviour {
 
 	private SpriteRenderer Sprite;
 	private Animator anim;
-	private string ladoAtual = "E"; //E == Esquerda D == Direita
+	private string ladoAtual = "E"; //E == Esquerda D == Direita;
 
 	// Use this for initialization
 	void Start () {
