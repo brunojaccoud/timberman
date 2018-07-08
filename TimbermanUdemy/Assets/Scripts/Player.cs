dcce76bfdc4f69ee0c2f727e4dab233a606979df
brunojaccoud <bruno_jaccoud@hotmail.com>
2018-07-08 00:25:06 -0300

    class player

---
+++ TimbermanUdemy/Assets/Scripts/Player.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+	private SpriteRenderer Sprite;
+	private Animator anim;
+	private string ladoAtual = "E"; //E == Esquerda D == Direita
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
