d9f63fa8ad45fd230938ebab69748cf7623adba0
brunojaccoud <bruno_jaccoud@hotmail.com>
2018-07-11 00:51:44 -0300

    ajuste

--- TimbermanUdemy/Assets/Scripts/Player.cs
+++ TimbermanUdemy/Assets/Scripts/Player.cs
@@ -4,13 +4,14 @@ using UnityEngine;
 
 public class Player : MonoBehaviour {
 
-	private SpriteRenderer Sprite;
+	private SpriteRenderer sprite;
 	private Animator anim;
 	private string ladoAtual = "E"; //E == Esquerda D == Direita;
 
 	// Use this for initialization
 	void Start () {
-		
+		sprite = GetComponent<SpriteRenderer> ();
+		anim = GetComponent<Animator> ();
 	}
 	
 	// Update is called once per frame
